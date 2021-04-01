f14876cad02c639b7ef2eb041ff81a26b4f2e276
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-04-01 16:16:04 -0300

    Activity Navigation teste adicionado com sucesso.

--- app/src/androidTest/java/com/lucascabral/espressouitest/MainActivityTest.kt
+++ app/src/androidTest/java/com/lucascabral/espressouitest/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.lucascabral.espressouitest
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -31,5 +32,12 @@ class MainActivityTest {
             .check(matches(isDisplayed()))
         onView(withId(R.id.mainActivityNextButton)) // method 2
             .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun test_navSecondaryActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.mainActivityNextButton)).perform(click())
+        onView(withId(R.id.secondaryActivity)).check(matches(isDisplayed()))
     }
 }
