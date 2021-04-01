68d657d1477ea5443acf2ccb73e65b40e445ca6c
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-04-01 17:06:50 -0300

    Criado a classe ActivityTestSuite para executar os testes das classes MainActivityTest e SecondaryActivityTest.

---
+++ app/src/androidTest/java/com/lucascabral/espressouitest/ActivityTestSuite.kt
@@ -0,0 +1,12 @@
+package com.lucascabral.espressouitest
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    SecondaryActivityTest::class
+)
+class ActivityTestSuite {
+}
